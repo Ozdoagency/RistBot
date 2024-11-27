68a1c9b9d516d94240aefc50fef668ea5a40c2a4
Ozdoagency <ozdoagency@gmail.com>
2024-11-27 19:17:33 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -10,7 +10,7 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: '/Ozdoagency/RistBot/bot.log' }),
+    new winston.transports.File({ filename: 'bot.log' }), // Измените путь здесь
   ],
 });
 
